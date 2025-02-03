61122e9a39f99a45a002a186d225659ecdf09863
hairinwind <dong_yao@hotmail.com>
2025-02-02 19:40:42 -0800

    cleanup

--- tab-switcher/background.js
+++ tab-switcher/background.js
@@ -25,30 +25,12 @@ const queue = new TwoItemQueue();
 chrome.tabs.onActivated.addListener((activeInfo) => {
   console.log("abcdefg..." + activeInfo.tabId);
   
-  queue.add(activeInfo.tabId)
-
-  // 向所有 popup 发送更新
-  lastLog = `tab activated！ ${activeInfo.tabId }`;
-  chrome.runtime.sendMessage({ action: "log", message: activeInfo.previousTabId});
+  queue.add(activeInfo.tabId);
 });
 
-
-let lastLog = ""; // 记录最近的快捷键触发日志
 
 chrome.commands.onCommand.addListener((command) => {
   if (command === "switch-tab") {
-    lastLog = `快捷键触发成功！ ${new Date().toLocaleTimeString()}`;
-
     chrome.tabs.update(queue.getFirstItem(), { active: true });
-
-    // 向所有 popup 发送更新
-    chrome.runtime.sendMessage({ action: "log", message: lastLog });
-  }
-});
-
-// 允许 popup 获取最近的日志
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "get_log") {
-    sendResponse({ log: lastLog });
   }
 });
--- tab-switcher/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    alert(message.command);
-    if (message.command === "switch-tab") {
-      alert("快捷键触发成功！");
-    }
-  });
